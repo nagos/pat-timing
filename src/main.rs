6c9f9786288656b50ae1752825b89543a92f3752
Vladimir Yakovlev <nagos@inbox.ru>
2023-01-19 17:46:38 +0300

    Модуль чтения данных

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod tsdump;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/tsdump.rs
@@ -0,0 +1,91 @@
+use std::io::Read;
+use std::iter::Iterator;
+use std::fs::File;
+
+pub struct TsDump {
+    fh: File,
+}
+
+const BLOCK_SIZE: usize = 7;
+const HEADER_SIZE: usize = 4;
+const TS_OFFSET: usize = BLOCK_SIZE*HEADER_SIZE;
+const MAX_TS: i32  = 0x7FFFFFF;
+
+impl Iterator for TsDump {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        let data: &mut [u8] = &mut [0; BLOCK_SIZE*HEADER_SIZE+4];
+        match self.fh.read_exact(data){
+            Ok(_) => {},
+            Err(_) => return None,
+        }
+        self.parse_block(data);
+        dbg!(data);
+        Some(1)
+    }
+}
+
+fn extract_ts(data: &[u8]) -> u32 {
+    let ts_data = &data[TS_OFFSET..TS_OFFSET+4];
+     (ts_data[3] as u32) <<0
+        | (ts_data[2] as u32) <<8
+        | (ts_data[1] as u32) <<16
+        | (ts_data[0] as u32) <<24
+}
+
+fn ts_diff(ts1: u32, ts2: u32) -> i32 {
+    let d = ts1 as i32 - ts2 as i32;
+    if d > (MAX_TS+1)/2 {
+        return d as i32 -(MAX_TS+1) as i32;
+    }
+    if d < -((MAX_TS+1)/2) {
+        return d - (MAX_TS+1);
+    }
+    return d;
+}
+
+impl TsDump {
+    fn parse_block(&self, data: &[u8]) {
+        let ts = extract_ts(data);
+    }
+
+    fn extract_ts(&self, data: &[u8]) -> u32 {
+        let ts_data = &data[TS_OFFSET..TS_OFFSET+4];
+         (ts_data[3] as u32) <<0
+            | (ts_data[2] as u32) <<8
+            | (ts_data[1] as u32) <<16
+            | (ts_data[0] as u32) <<24
+    }
+}
+
+impl TsDump {
+    pub fn build(fname: &str) -> TsDump {
+        let fh = File::open(fname).unwrap();
+        TsDump{fh}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let mut i = TsDump::build("testdata/record_1.ts");
+        dbg!(i.next());
+        dbg!(i.next());
+        dbg!(i.next());
+    }
+
+    #[test]
+    fn ts_diff_test() {
+        let t1 :u32 = 123;
+        let t2 :u32 = 23;
+        let t3 :u32 = (MAX_TS-1) as u32;
+        let d1 = ts_diff(t1, t2);
+        assert_eq!(d1, 100);
+        let d2 = ts_diff(t2, t1);
+        assert_eq!(d2, -100);
+        let d3 = ts_diff(t3, t2);
+        assert_eq!(d3, -25);
+    }
+}
