3a31d0fa40cc720d581b746c3e49edef97e0a136
Vladimir Yakovlev <nagos@inbox.ru>
2023-01-27 13:07:48 +0300

    Подключена буферизация чтения

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use pat_timing::tsdump;
 use std::fs::File;
+use std::io::BufReader;
 
 fn check_pair((a, b): (pat_timing::PacketData, pat_timing::PacketData)) {
     let diff = tsdump::ts_to_us(tsdump::ts_diff(a.0, b.0));
@@ -14,8 +15,8 @@ fn main() {
     let fname1 = args.next().unwrap();
     let fname2 = args.next().unwrap();
 
-    let fh1 = File::open(fname1).unwrap();
-    let fh2 = File::open(fname2).unwrap();
+    let fh1 = BufReader::new(File::open(fname1).unwrap());
+    let fh2 = BufReader::new(File::open(fname2).unwrap());
 
     let it1 = tsdump::TsDump::build(fh1).flat_map(pat_timing::block_process).filter(pat_timing::filter_data);
     let it2 = tsdump::TsDump::build(fh2).flat_map(pat_timing::block_process).filter(pat_timing::filter_data);
